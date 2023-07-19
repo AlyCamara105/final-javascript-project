3666f564861d94b8fa0aee3fcd3a53f6666f10ab
Aly <alcamara237@gmail.com>
2023-07-19 17:54:47 -0400

    Finished the website

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
 let moviesWrapper = document.querySelector(".movies__wrapper");
 let searchInput = document.querySelector(".search__input");
 let api = "https://omdbapi.com/?apikey=c746e04e&";
+let loadingImg = document.querySelector(".loading__img");
+let inputFeedback = document.querySelector(".input__feedback");
 
-async function searchButtonClicked(event) {
+async function searchButtonClicked() {
   const res = await fetch(api + `s=${searchInput.value}`);
   const data = await res.json();
 
-  if (data.Search) {
-    renderMovies(data.Search);
-  }
+  inputFeedback.innerHTML = `Search Results for: <span class="text-purple-900">${searchInput.value}</span>`;
+
+  renderMovies(data.Search);
+}
+
+async function enterKeyPressed(event) {
+  if (event.key === "Enter") searchButtonClicked();
 }
 
 function renderMovies(movies) {
-  newMoviesHtml = movies
-    .map(
-      (movie) => `
+  let newMoviesHtml = "";
+
+  loadingImg.classList.remove("hidden");
+  loadingImg.classList.add("block");
+  moviesWrapper.innerHTML = newMoviesHtml;
+
+  if (movies) {
+    newMoviesHtml = movies
+      .map(
+        (movie) => `
     <div class="flex flex-col mx-1 justify-center items-center">
     <p class="text-sm">${movie.Title}</p>
     <img class="max-h-40 object-contain" src=${movie.Poster} alt="" />
     </div>
   `
-    )
-    .slice(0, 10)
-    .join("");
-  moviesWrapper.innerHTML = newMoviesHtml;
+      )
+      .slice(0, 5)
+      .join("");
+  }
+
+  if (newMoviesHtml === "") {
+    newMoviesHtml = `<div class="flex text-center"><p class="font-semibold">Couldn't find any matches for your related search.</p></div>`;
+  }
+
+  setTimeout(() => {
+    loadingImg.classList.remove("block");
+    loadingImg.classList.add("hidden");
+    moviesWrapper.innerHTML = newMoviesHtml;
+  }, 1000);
 }
